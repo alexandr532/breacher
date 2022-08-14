1bf815782e11ee4e190cc4f2a73dc983fe62128c
Alexandr <alexandr532@proton.me>
2022-08-14 13:50:11 +0200

    Added auth handler in transfer layer

--- packages/breacher-transfer/src/BreacherTransfer.ts
+++ packages/breacher-transfer/src/BreacherTransfer.ts
@@ -9,12 +9,16 @@
 import http from 'http';
 import * as io from 'socket.io';
 import { Auth } from './Auth';
+import { Connections } from './Connections';
 
 // TODO Should be moved to shared config.
 const BREACHER_SERVER_PORT = 3003;
 
 export class BreacherTransfer {
   private _auth: Auth;
+
+  private _connetions: Connections;
+
   // Socket server is not defined if server is not running.
   private _io!: io.Server;
 
@@ -24,6 +28,7 @@ export class BreacherTransfer {
   public constructor() {
     this._auth = new Auth();
     this._startSocketServer();
+    this._connetions = new Connections(this._io);
   }
 
   // Adds basic socket listeners to handle connects and disconnects.
@@ -33,27 +38,40 @@ export class BreacherTransfer {
     this._io.on('connection', this._handleConnection)
   }
   
-  private _addMoreListeners(socket: io.Socket): void {
+  private _addProtectedListeners(socket: io.Socket): void {
     // TODO add listeners to request data
   }
 
   private _handleConnection = (socket: io.Socket): void => {
     socket.on('disconnect', this._handleDisconnect);
-    socket.on('auth', async (token: string): Promise<void> => {
-      // TODO create new connection if token is not provided
+
+    socket.on('auth', async (token?: string): Promise<void> => {
+      // Revoke token is set here if new auth requested
+      let revokeToken: string | undefined = token == null ? this._auth.token(socket.id) : undefined;
+      // Token as newly created revoke token, or the one that we got from client
+      token = token == null ? revokeToken : token;
+      let connectionId: string;
       try {
-        const hashId: string = await this._auth.verify(token);
-        const revoke: string = await this._auth.revoke(token);
-        // TODO Connection revoke for hashId with new socket
-        this._addMoreListeners(socket);
-        socket.emit('token', revoke);
+        // If token is valid will wait for connection id and catch will not happen
+        connectionId = await this._auth.verify(token!);
+        // Revoke token will be updated if not just created for new auth
+        revokeToken = revokeToken == null ? await this._auth.revoke(token!) : revokeToken;
       } catch (e) {
+        // Provided token is invalid
         socket.emit('auth-error', e);
-        // TODO create new connection
+        // Creates new token and saves it as revoke token
+        revokeToken = this._auth.token(socket.id);
+        // This can not fail as just created token is always valid
+        connectionId = await this._auth.verify(revokeToken);
+      } finally {
+        // At this point connection id, revoke token and socket are always defined
+        this._connetions.register(connectionId!, socket);
+        this._addProtectedListeners(socket);
+        socket.emit('token', revokeToken);
       }
     });
   }
-  
+
   private _handleDisconnect = (reason: string): void => {
     console.log(`User disconnected because of ${reason}`);
   }
